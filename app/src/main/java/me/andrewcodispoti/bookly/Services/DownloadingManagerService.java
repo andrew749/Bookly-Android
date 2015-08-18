98d5d9cca543373cc05b636ed350064f1378b63b
Andrew Codispoti <andrewcod749@gmail.com>
2015-08-17 21:13:16 -0400

    Fixed dependency for ttorrent.

--- app/src/main/java/me/andrewcodispoti/bookly/Services/DownloadingManagerService.java
+++ app/src/main/java/me/andrewcodispoti/bookly/Services/DownloadingManagerService.java
@@ -4,7 +4,10 @@ import android.app.IntentService;
 import android.app.Service;
 import android.content.Intent;
 import android.content.IntentSender;
+import android.os.AsyncTask;
 import android.util.Log;
+
+import java.io.File;
 
 /**
  * Created by andrewcodispoti on 2015-08-17.
@@ -20,6 +23,19 @@ public class DownloadingManagerService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        Log.d("BOOKLY", "got intent");
+
     }
+
+    class TorrentFileDownloader extends AsyncTask<String, Void, File>{
+        @Override
+        protected File doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(File file) {
+            super.onPostExecute(file);
+        }
+    }
+
 }
